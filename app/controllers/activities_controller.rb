6a2403affb2a6145c5ae497ea26ae6a4870a4ef2
brad-bb-bell <brad.bell5@montana.edu>
2022-10-10 17:37:54 -0600

    starts testing routes through routes.http rest extenstion

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,28 +2,30 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = Activity.all
-    render template: "products/index"
+    # render template: "products/index"
+    render json: { activities: @activities.as_json }
   end
 
   def show
     @activity = Activity.find_by(id: params["id"])
-    render template: "activities/show"
+    render json: { activities: @activity.as_json }
   end
 
   def create
-    @activity = Activity.new(
+    activity = Activity.new(
       name: params["name"],
     )
-    if @activity.save
-      render template: "activities/show"
-    else render json: { errors: @activity.errors.full_messages }, status: :unprocessable_entity
+    if activity.save
+      render json: { activity: activity.as_json }
+    else render json: { errors: activity.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
     activity = Activity.find_by(id: params["id"])
     activity.name = params["name"] || activity.name
     if activity.save
-      render template: "activities/show"
+      render json: { activity: activity.as_json }
     else
       render json: { errors: activity.errors.full_messages }, status: :unprocessable_entity
     end
@@ -36,4 +38,3 @@ class ActivitiesController < ApplicationController
   end
 
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ get "/activities" => "activities#index"
 get "/activities/:id" => "activities#show"
 post "/activities" => "activities#create"
 patch "/activities/:id" => "activities#update"
+delete "/activities/:id" => "activities#destroy"
 
 ###Did_It routes
 get "/did_its" => "did_its#index"
 post "/did_its" => "did_its#create"
+delete "/did_its" => "did_its#destroy"
 
 end
