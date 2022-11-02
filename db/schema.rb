ba0ffb81533f59baeacd0c2f4adff440173bb61b
brad-bb-bell <brad.bell5@montana.edu>
2022-11-02 14:40:47 -0600

    adds streak column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_22_201059) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_203602) do
   create_table "activities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_22_201059) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "streak", default: 0
   end
 
 end
