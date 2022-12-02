3df941940dd2fb0bf12ac5ef24b982bc1af5b5d3
brad-bb-bell <brad.bell5@montana.edu>
2022-12-02 15:55:36 -0700

    updates seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,6 +44,7 @@ DidIt.create!(user_id: 1, activity_id: 1, date: "2022-11-22")
 DidIt.create!(user_id: 1, activity_id: 1, date: "2022-11-28")
 DidIt.create!(user_id: 1, activity_id: 1, date: "2022-11-29")
 DidIt.create!(user_id: 1, activity_id: 1, date: "2022-11-30")
+DidIt.create!(user_id: 1, activity_id: 1, date: "2022-12-01")
 
 DidIt.create!(user_id: 1, activity_id: 2, date: "2022-10-05")
 DidIt.create!(user_id: 1, activity_id: 2, date: "2022-10-16")
